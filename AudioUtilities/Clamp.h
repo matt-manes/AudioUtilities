129b8ab11b49520fdff1da184ee27e32689e27d2
matt manes <mattmanes@pm.me>
2024-08-28 17:02:34 -0500

    fix: fix clamp not working correctly when `min` and `max` are swapped

--- AudioUtilities/Clamp.h
+++ AudioUtilities/Clamp.h
@@ -1,23 +1,32 @@
 #pragma once
 
 #include "Range.h"
+#include <cmath>
 
 namespace AudioUtilities
 {
     namespace Clamp
     {
-        // Clamp `val` within the range `min` to `max`.
-        template <typename T> inline T clamp(T val, T min, T max)
+        // Clamp `val` within `range`.
+        template <typename T> inline T clamp(T value, Range::Range<T> range)
         {
-            if (val < min) { return min; }
-            if (max < val) { return max; }
-            return val;
+            if (range.contains(value)) { return value; }
+
+            // By using absolute value of deltas we account for min value being
+            // greater than max
+            T startDelta = std::abs(value - range.getStart());
+            T stopDelta = std::abs(value - range.getStop());
+            // `value` is outside of range and closer to `range.start`
+            if (startDelta < stopDelta) { return range.getStart(); }
+            // `value` is outside of range and closer to `range.stop`
+            return range.getStop();
         }
 
-        // Clamp `val` within `range`.
-        template <typename T> inline T clamp(T val, Range::Range<T> range)
+        // Clamp `value` within the range `min` to `max`.
+        template <typename T> inline T clamp(T value, T min, T max)
         {
-            return clamp(val, range.getStart(), range.getStop());
+            Range::Range<T> range(min, max);
+            return clamp(value, range);
         }
     } // namespace Clamp
 } // namespace AudioUtilities
--- ClampTest/ClampTest.cpp
+++ ClampTest/ClampTest.cpp
@@ -13,11 +13,19 @@ TEST_CLASS(ClampTest)
         Assert::AreEqual(0.0f, AudioUtilities::Clamp::clamp(-1.0f, 0.0f, 1.0f), 0.001f);
         Assert::AreEqual(0.5f, AudioUtilities::Clamp::clamp(0.5f, 0.0f, 1.0f), 0.001f);
         Assert::AreEqual(1.0f, AudioUtilities::Clamp::clamp(1.5f, 0.0f, 1.0f), 0.001f);
+
+        Assert::AreEqual(0.0f, AudioUtilities::Clamp::clamp(-1.0f, 1.0f, 0.0f), 0.001f);
+        Assert::AreEqual(0.5f, AudioUtilities::Clamp::clamp(0.5f, 1.0f, 0.0f), 0.001f);
+        Assert::AreEqual(1.0f, AudioUtilities::Clamp::clamp(1.5f, 1.0f, 0.0f), 0.001f);
     }
 
     TEST_METHOD(TestClampRange)
     {
         AudioUtilities::Range::Range<float> range(-1.0f, 1.0f);
+        Assert::AreEqual(-1.0f, AudioUtilities::Clamp::clamp(-5.0f, range));
+        Assert::AreEqual(0.0f, AudioUtilities::Clamp::clamp(0.0f, range));
+        Assert::AreEqual(1.0f, AudioUtilities::Clamp::clamp(100.0f, range));
+        range.reverse();
         Assert::AreEqual(-1.0f, AudioUtilities::Clamp::clamp(-5.0f, range));
         Assert::AreEqual(0.0f, AudioUtilities::Clamp::clamp(0.0f, range));
         Assert::AreEqual(1.0f, AudioUtilities::Clamp::clamp(100.0f, range));
