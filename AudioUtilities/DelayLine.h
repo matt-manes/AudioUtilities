aec66d2bf352bf0edb3d5ff1823a7df95c97caca
matt manes <mattmanes@pm.me>
2024-09-01 13:32:58 -0500

    docs: add doc comments

--- AudioUtilities/DelayLine.h
+++ AudioUtilities/DelayLine.h
@@ -8,6 +8,10 @@ namespace AudioUtilities
 {
     namespace DelayLine
     {
+        /*
+        A variable speed delay line inheriting from `CircleBuff` in
+        "CircleBuff.h".
+        */
         class DelayLine : public CircleBuff::CircleBuff<float>
         {
           public:
@@ -16,9 +20,12 @@ namespace AudioUtilities
 
             DelayLine(float delayMilliseconds, int sampleRate);
 
-            // Set to `false` to handle updating the read index manually
+            // Set to `false` to handle updating the read index manually.
             bool autoIncrementReadex = true;
 
+            // Returns the sample at the current read index.
+            // Increments the read index after if `autoIncrementReadex` is
+            // `true`.
             float read();
 
             int getSampleRate() const;
@@ -29,16 +36,22 @@ namespace AudioUtilities
 
             void setDelayMilliseconds(float ms);
 
+            // Returns a pointer to the read `Index` object.
             Index::Index *getReadex();
 
+            // The amount the read `Index` is moved when `autoIncrementReadex`
+            // is `true`.
             float getReadSpeed() const;
 
+            // Set the amount the read `Index` is moved when
+            // `autoIncrementReadex` is `true`.
             void setReadSpeed(float speed);
 
           protected:
 
             void resize(int size) override;
 
+            // Increment `readex` according to `readspeed`.
             virtual void incrementRead();
 
             Index::Index readex;
