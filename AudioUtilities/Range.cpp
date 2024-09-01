e0cd290dd70760ccd254eaa73930ae1b39104ac0
matt manes <mattmanes@pm.me>
2024-09-01 13:13:51 -0500

    refactor: explicitly instantiate template for only int and float

---
+++ AudioUtilities/Range.cpp
@@ -0,0 +1,171 @@
+#include "Range.h"
+
+template <class T> AudioUtilities::Range::Range<T>::Range()
+{
+    setBounds((T)0, (T)1);
+}
+
+template <class T> AudioUtilities::Range::Range<T>::Range(T start, T stop)
+{
+    setBounds(start, stop);
+}
+
+template <class T>
+bool AudioUtilities::Range::Range<T>::operator==(const Range &other) const
+{
+    return (start == other.start) && (stop == other.stop);
+}
+
+template <class T>
+void AudioUtilities::Range::Range<T>::setBounds(T start, T stop)
+{
+    this->start = start;
+    this->stop = stop;
+    parseLowerUpper();
+    calcDelta();
+}
+
+template <class T> void AudioUtilities::Range::Range<T>::setStart(T val)
+{
+    start = val;
+    parseLowerUpper();
+    calcDelta();
+}
+
+template <class T> void AudioUtilities::Range::Range<T>::setStop(T val)
+{
+    stop = val;
+    parseLowerUpper();
+    calcDelta();
+}
+
+template <class T> T AudioUtilities::Range::Range<T>::getStart() const
+{
+    return start;
+}
+
+template <class T> T AudioUtilities::Range::Range<T>::getStop() const
+{
+    return stop;
+}
+
+template <class T> T AudioUtilities::Range::Range<T>::getLower() const
+{
+    return std::fmin(start, stop);
+}
+
+template <class T> T AudioUtilities::Range::Range<T>::getUpper() const
+{
+    return std::fmax(start, stop);
+}
+
+template <class T> T AudioUtilities::Range::Range<T>::getDelta() const
+{
+    return delta;
+}
+
+template <class T> T AudioUtilities::Range::Range<T>::getAbsDelta() const
+{
+    return std::abs(delta);
+}
+
+template <class T> void AudioUtilities::Range::Range<T>::reverse()
+{
+    float tmp = start;
+    start = stop;
+    stop = tmp;
+    parseLowerUpper();
+    calcDelta();
+}
+
+template <class T>
+AudioUtilities::Range::Range<T> AudioUtilities::Range::Range<T>::getReversed(
+) const
+{
+    return Range(stop, start);
+}
+
+template <class T> bool AudioUtilities::Range::Range<T>::isNegative() const
+{
+    return delta < 0.0f;
+}
+
+template <class T>
+float AudioUtilities::Range::Range<T>::getStepSize(int numSteps)
+{
+    return delta / numSteps;
+}
+
+template <class T>
+int AudioUtilities::Range::Range<T>::getNumSteps(float stepSize)
+{
+    return std::abs((int)std::ceil(delta / stepSize));
+}
+
+template <class T>
+bool AudioUtilities::Range::Range<T>::contains(
+    float val, Inclusion boundsInclusion
+)
+{
+    switch (boundsInclusion)
+    {
+        case Both: return inclusion(val);
+        case None: return noInclusion(val);
+        case Lower: return lowerInclusion(val);
+        case Upper: return upperInclusion(val);
+        case Start:
+        {
+            if (isNegative()) { return upperInclusion(val); }
+            return lowerInclusion(val);
+        }
+        case Stop:
+        {
+            if (isNegative()) { return lowerInclusion(val); }
+            return upperInclusion(val);
+        }
+    }
+}
+
+template <class T> void AudioUtilities::Range::Range<T>::calcDelta()
+{
+    delta = stop - start;
+}
+
+template <class T> void AudioUtilities::Range::Range<T>::parseLowerUpper()
+{
+    if (start < stop)
+    {
+        lower = start;
+        upper = stop;
+    }
+    else
+    {
+        lower = stop;
+        upper = start;
+    }
+}
+
+template <class T> bool AudioUtilities::Range::Range<T>::inclusion(float val)
+{
+    return (lower <= val) && (val <= upper);
+}
+
+template <class T>
+bool AudioUtilities::Range::Range<T>::lowerInclusion(float val)
+{
+    return (lower <= val) && (val < upper);
+}
+
+template <class T>
+bool AudioUtilities::Range::Range<T>::upperInclusion(float val)
+{
+    return (lower < val) && (val <= upper);
+}
+
+template <class T> bool AudioUtilities::Range::Range<T>::noInclusion(float val)
+{
+    return (lower < val) && (val < upper);
+}
+
+template AudioUtilities::Range::Range<float>;
+template AudioUtilities::Range::Range<int>;
--- AudioUtilities/Range.h
+++ AudioUtilities/Range.h
@@ -24,79 +24,50 @@ namespace AudioUtilities
         {
           public:
 
-            Range() { setBounds((T)0, (T)1); };
+            Range();
 
-            Range(T start, T stop) { setBounds(start, stop); }
+            Range(T start, T stop);
 
-            bool operator==(const Range &other) const
-            {
-                return (start == other.start) && (stop == other.stop);
-            }
+            bool operator==(const Range &other) const;
 
-            void setBounds(T start, T stop)
-            {
-                this->start = start;
-                this->stop = stop;
-                parseLowerUpper();
-                calcDelta();
-            }
+            void setBounds(T start, T stop);
 
-            void setStart(T val)
-            {
-                start = val;
-                parseLowerUpper();
-                calcDelta();
-            }
+            void setStart(T val);
 
-            void setStop(T val)
-            {
-                stop = val;
-                parseLowerUpper();
-                calcDelta();
-            }
+            void setStop(T val);
 
-            T getStart() const { return start; }
+            T getStart() const;
 
-            T getStop() const { return stop; }
+            T getStop() const;
 
             // Returns the lower of `start` and `stop`
-            T getLower() const { return std::fmin(start, stop); }
+            T getLower() const;
 
             // Returns the higher of `start` and `stop`
-            T getUpper() const { return std::fmax(start, stop); }
+            T getUpper() const;
 
             // Returns the difference between `stop` and `start`.
-            T getDelta() const { return delta; }
+            T getDelta() const;
 
             // Returns the absolute value of the delta
-            T getAbsDelta() const { return std::abs(delta); }
+            T getAbsDelta() const;
 
             // Swaps the `start` and `stop` values of this instance.
-            void reverse()
-            {
-                float tmp = start;
-                start = stop;
-                stop = tmp;
-                parseLowerUpper();
-                calcDelta();
-            }
+            void reverse();
 
             // Returns a new `Range` with opposite `start` and `stop` values.
-            Range getReversed() const { return Range(stop, start); }
+            Range getReversed() const;
 
             // Returns `true` if `start` is greater than `stop`.
-            bool isNegative() const { return delta < 0.0f; }
+            bool isNegative() const;
 
             // Returns the size of each step if this range were divided into
             // `numSteps`.
-            float getStepSize(int numSteps) { return delta / numSteps; }
+            float getStepSize(int numSteps);
 
             // Returns the number of steps required to get from `start` to
             // `stop` with `stepSize`.
-            int getNumSteps(float stepSize)
-            {
-                return std::abs((int)std::ceil(delta / stepSize));
-            }
+            int getNumSteps(float stepSize);
 
             /*
             Returns `true` if `value` is within this range.
@@ -115,26 +86,7 @@ namespace AudioUtilities
             */
             bool contains(
                 float val, Inclusion boundsInclusion = Inclusion::Both
-            )
-            {
-                switch (boundsInclusion)
-                {
-                    case Both: return inclusion(val);
-                    case None: return noInclusion(val);
-                    case Lower: return lowerInclusion(val);
-                    case Upper: return upperInclusion(val);
-                    case Start:
-                    {
-                        if (isNegative()) { return upperInclusion(val); }
-                        return lowerInclusion(val);
-                    }
-                    case Stop:
-                    {
-                        if (isNegative()) { return lowerInclusion(val); }
-                        return upperInclusion(val);
-                    }
-                }
-            }
+            );
 
           private:
 
@@ -144,46 +96,22 @@ namespace AudioUtilities
             T upper;
             T delta;
 
-            void calcDelta() { delta = stop - start; }
+            void calcDelta();
 
             // Sets `upper` and `lower` to appropriate `start`/`stop` values.
-            void parseLowerUpper()
-            {
-                if (start < stop)
-                {
-                    lower = start;
-                    upper = stop;
-                }
-                else
-                {
-                    lower = stop;
-                    upper = start;
-                }
-            }
+            void parseLowerUpper();
 
             // Whether `value` is in this range, inclusive of bounds.
-            bool inclusion(float val)
-            {
-                return (lower <= val) && (val <= upper);
-            }
+            bool inclusion(float val);
 
             // Whether `value` is in this range, inclusive of lower bound only.
-            bool lowerInclusion(float val)
-            {
-                return (lower <= val) && (val < upper);
-            }
+            bool lowerInclusion(float val);
 
             // Whether `value` is in this range, inclusive of upper bound only.
-            bool upperInclusion(float val)
-            {
-                return (lower < val) && (val <= upper);
-            }
+            bool upperInclusion(float val);
 
             // Whether `value` is in this range, exclusive of bounds.
-            bool noInclusion(float val)
-            {
-                return (lower < val) && (val < upper);
-            }
+            bool noInclusion(float val);
         };
     } // namespace Range
 } // namespace AudioUtilities
